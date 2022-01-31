10fb7b24ff56b11a249951b4389115846eaffca0
srodrigo28 <rodrigoexer1@gmail.com>
2022-01-31 20:51:29 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 
 import './App.css'
-import { UseEffect5 } from './UseEffect/UseEffect5';
+import { useState } from 'react'
 // https://alunos.b7web.com.br/curso/reactjs/react-ciclo-de-vida-do-componente
 function App() {
-  
+  const [movies, setMovies] = useState([]);
+
+  // https://api.b7web.com.br/cinema/
   return (
     <div className='container'>
-      <UseEffect5 />
+      
+      <button className='btn btn-outline-primary mt-3'>Carregar Filmes</button>
+      <br />
+      <p className='alert alert-success mt-3'>Total de Filmes: {movies.length}</p>
+      <div>
+
+      </div>
     </div>
   );
 }
